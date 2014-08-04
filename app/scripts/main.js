3d8845be713e792b3410b4d883c2a44a376add52
Derek Lieu <derek@developmentseed.org>
2014-08-04 15:47:35 -0400

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Air, $*/
+
+
+window.Air = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Air.init();
+});
