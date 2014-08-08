595f8291dbcd869e10263eeb2b806f3969f4b563
Derek Lieu <derek@developmentseed.org>
2014-08-08 10:16:08 -0400

    Commiting Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -200,6 +200,7 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
                         'styles/fonts/{,*/}*.*',
+                        'local/*.json',
                     ]
                 }]
             }
